5e87f4f0b29e5a5558410663919b8d335f480a7f
PONYASHIN <102251712+PONYASHIN@users.noreply.github.com>
2025-06-03 23:30:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('downloadbutton').addEventListener('click', function() {
+window.location.href = 'https://github.com/PONYASHIN/XPonTweak/raw/refs/heads/main/XPonTweak.exe';
+    // Или любое другое действие:
+    // this.textContent = 'Нажато!';
+    // document.body.style.backgroundColor = 'lightblue';
+  });
